0f718b0bcdbe53f2c19a3aa8c93498ec6ccfab21
julesgaborit <jules.gaborit@my-digital-school.org>
2023-03-09 16:52:33 +0100

    v2testunitaire

--- sum.js
+++ sum.js
@@ -8,7 +8,35 @@ class User {
     }
   }
 // const u = new User('Doe', 25, 'John', 'rt@gmail.com');
+function name(firstname) {
+    if (firstname === '' || firstname === undefined|| firstname === null) {
+        throw new Error('Invalid firstname');
+    }
+    return firstname;
+}
+function firstname(lastname) {
+    if (lastname === '' || lastname === null) {
+        throw new Error('Invalid lastname');
+    }
+    return lastname;
+}
 
+function age(age) {
+    if (age < 13) {
+        throw new Error('Invalid age');
+    }
+    return age;
+
+}
+
+function email(email) {
+    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!regex.test(email)) {
+        throw new Error('Invalid email');
+    }
+    return email;
+
+}
 function isValide(firstname,lastname,age,email) {
     let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (age < 13) {
@@ -25,5 +53,9 @@ function isValide(firstname,lastname,age,email) {
 }
 module.exports = {
     isValide,
-    User
+    User,
+    name,
+    age,
+    email,
+    firstname
 }
--- sum.test.js
+++ sum.test.js
@@ -1,23 +1,32 @@
-const z = require('./sum');
-  test('test firstname', () => {
-    expect(() => z.isValide('', 'Doe', 25, 'john.doe@example.com')).toThrow();
-  });
+const z = require("./sum");
 
-  test('test lastname', () => {
-    expect(() => {
-      z.isValide('John', '', 25, 'joh.doe@example.com');
-    }).toThrow();
-  });
-
-  test('test email', () => {
-    expect(() => {
-      z.isValide('John', 'Doe', 25, 'invalid-email');
-    }).toThrow();
-  });
-
-  test('test age', () => {
-    expect(() => {
-      z.isValide('John', 'Doe', 12, 'john.doe@example.com');
-    }).toThrow();
-  });
-
+test("test isValid", () => {
+  expect(() => z.isValide("", "Doe", 25, "john.doe@example.com")).toThrow();
+});
+test("test lastname", () => {
+  expect(() => z.name("")).toThrow();
+});
+test("test firstname", () => {
+  expect(() => z.firstname("")).toThrow();
+});
+test("test age", () => {
+  expect(() => z.age(12)).toThrow();
+});
+test("test email", () => {
+  expect(() => z.email("john.doe@example")).toThrow();
+});
+test('should first', () => { 
+    expect(z.name('john')).toBe('john');
+ })
+    test('should last', () => {
+        expect(z.firstname('doe')).toBe('doe');
+    }
+    )
+    test('should age', () => {
+        expect(z.age(25)).toBe(25);
+    }
+    )
+    test('should email', () => {
+        expect(z.email('de@gmail.com')).toBe('de@gmail.com');
+    }
+    )
